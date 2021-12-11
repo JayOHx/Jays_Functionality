c95496c3b18534f420a970661ed4a42b9328f802
JayOHx <83920038+JayOHx@users.noreply.github.com>
2021-12-11 02:40:07 +0000

    Update client.lua

--- client.lua
+++ client.lua
@@ -30,6 +30,28 @@ Citizen.CreateThread(function()
     end
 end)
 
+Citizen.CreateThread(function()
+    local dict = "missminuteman_1ig_2"
+    
+	RequestAnimDict(dict)
+	while not HasAnimDictLoaded(dict) do
+		Citizen.Wait(100)
+	end
+    local handsup = false
+	while true do
+		Citizen.Wait(0)
+		if IsControlJustPressed(1, Config.HandsKey) and Config.HandsUp then
+            if not handsup then
+                TaskPlayAnim(GetPlayerPed(-1), dict, "handsup_enter", 8.0, 8.0, -1, 50, 0, false, false, false)
+                handsup = true
+            else
+                handsup = false
+                ClearPedTasks(GetPlayerPed(-1))
+            end
+        end
+    end
+end)
+
 -- POPULATION CONTROL THINGS ARE HERE --
 
 DensityMultiplier = Config.DensityMultiplier
