4acdf5260b26c4ae48d4578f0c9addd7fe175fcc
JayOHx <83920038+JayOHx@users.noreply.github.com>
2021-12-11 02:39:52 +0000

    Update config.lua

--- config.lua
+++ config.lua
@@ -2,17 +2,23 @@
 Config = {}
 
 -- Debug options = 'client' prints to F8 consol, 'server' prints to server consol. Leave blank to disable.
-Config.Debug =                  'client'
+Config.Debug                    = 'client'
 
 -- Radio disable/enable for your city. --
-Config.DisableRadio =           true
+Config.DisableRadio             = true
 
 -- All below is the NPC control configs. --
-Config.DensityMultiplier =      0.0
-Config.AllowGarbage =           false
-Config.AllowPolice =            false
-Config.AllowBoatSpawns =        false
+Config.DensityMultiplier        = 0.0
+Config.AllowGarbage             = false
+Config.AllowPolice              = false
+Config.AllowBoatSpawns          = false
 
 -- Disable/Enable the ping command --
-Config.AllowPing =              false
+Config.AllowPing                = false
+
+-- Allow use of hands up? Defualt key map is 'X'.
+Config.HandsUp                  = true
+Config.HandsKey                 = 323
+
+-- https://docs.fivem.net/docs/game-references/controls/ -- USED FOR CHANING THE KEY BIND
 
