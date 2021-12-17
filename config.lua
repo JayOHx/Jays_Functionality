f7908b93023f340795b1f603e319a6dce2ecdd18
JayOHx <83920038+JayOHx@users.noreply.github.com>
2021-12-17 00:39:41 +0000

    v0.3a Config
    Updated to match the client.lua changes

--- config.lua
+++ config.lua
@@ -1,34 +1,21 @@
 
 Config = {}
 
--- Debug options = 'client' prints to F8 consol, 'server' prints to server consol. Leave blank to disable.
+-- Change certain commands here --
 Config.PingCommand              = 'ping'
 
--- Radio disable/enable for your city. --
-Config.DisableRadio             = true
-
--- All below is the NPC control configs. --
-Config.DensityMultiplier        = 0.1
-Config.AllowGarbage             = false
-Config.AllowPolice              = false
-Config.AllowBoatSpawns          = false
-Config.Allow911Calls            = false
-Config.RoamingPolice            = false
-
--- Disable/Enable the ping command --
+-- Disable or enable features here --
+Config.PointFinger              = true
+Config.AllowCrouch              = true
+Config.HandsUp                  = true
 Config.AllowPing                = true
 
--- Allow use of hands up? Defualt key map is 'X'.
-Config.HandsUp                  = true
+-- Key Bind Stuff Here --
 Config.HandsKey                 = 323
-
--- Point finger related options are here --
-Config.PointFinger              = true
-
--- Disable/Enable the crouch ability --
-Config.AllowCrouch              = true
 Config.CrouchKey                = 36
 
 -- https://docs.fivem.net/docs/game-references/controls/ -- USED FOR CHANING THE KEY BIND
 
-
+-- Made by JayOHx --
+-- Free open source --
+-- Sharing permitted DO NOT SELL/BUY THIS SCRIPT --
